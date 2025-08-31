9dc61b06d8e6941b53887c20ea6de665549fb58a
Developer-Jaber <jaberhossain.web.developer@gmail.com>
2025-09-01 00:57:40 +0400

    Update: enhance the custom alert for more reusable.

--- src/components/customComponents/CustomAlart.tsx
+++ src/components/customComponents/CustomAlart.tsx
@@ -1,4 +1,6 @@
-import { CheckCircle2Icon, XIcon } from 'lucide-react'
+'use client'
+
+import { AlertCircleIcon, CheckCircle2Icon, XIcon } from 'lucide-react'
 import { Alert, AlertDescription, AlertTitle } from '../ui/alert'
 import { useEffect } from 'react'
 
@@ -9,6 +11,7 @@ interface CustomAlartProps {
     title: string
     description: string
     duration?: number
+    variant: 'success' | 'error' | 'warning'
 }
 
 
@@ -17,7 +20,8 @@ export function CustomAlart ({
   description,
   isOpen,
   onClose,
-  duration = 5000
+  duration = 5000,
+  variant = 'success'
 }: CustomAlartProps) {
   useEffect(() => {
     if (isOpen && duration > 0) {
@@ -27,18 +31,42 @@ export function CustomAlart ({
     }
   }, [onClose, isOpen, duration])
 
+   const variantStyles = {
+    success: {
+      border: 'border-l-4 border-green-500',
+      bg: 'bg-green-50',
+      title: 'text-green-800',
+      description: 'text-green-700',
+      icon: <CheckCircle2Icon className="w-5 h-5 text-green-600" />
+    },
+    error: {
+      border: 'border-l-4 border-red-500',
+      bg: 'bg-red-50',
+      title: 'text-red-800',
+      description: 'text-red-700',
+      icon: <AlertCircleIcon className="w-5 h-5 text-red-600" />
+    },
+    warning: {
+      border: 'border-l-4 border-yellow-500',
+      bg: 'bg-yellow-50',
+      title: 'text-yellow-800',
+      description: 'text-yellow-700',
+      icon: <AlertCircleIcon className="w-5 h-5 text-yellow-600" />
+    }
+  }
+
   if (isOpen === false) return null
 
   return (
     <div className='top-4 right-4 slide-in-from-right-5 z-50 fixed animate-in'>
-      <Alert className='bg-white shadow-lg border-green-500 border-l-4'>
+      <Alert  className={`${variantStyles[variant].bg} ${variantStyles[variant].border} shadow-lg`}>
         <div className='flex items-start gap-3'>
-          <CheckCircle2Icon className='mt-0.5 w-5 h-5 text-green-600' />
+            {variantStyles[variant].icon}
           <div className='flex-1'>
-            <AlertTitle className='font-semibold text-green-800'>
+            <AlertTitle className={variantStyles[variant].title}>
               {title}
             </AlertTitle>
-            <AlertDescription className='text-green-700'>
+            <AlertDescription className={variantStyles[variant].description}>
               {description}
             </AlertDescription>
           </div>
